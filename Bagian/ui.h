62129ac8ee98f8c1dd827ff73e2df40017f097ba
Imam As-Shadiq <imamasshadiq1011@gmail.com>
2026-04-25 12:52:47 +0700

    add ui

---
+++ Bagian/ui.h
@@ -0,0 +1,13 @@
+#ifndef UI_H
+#define UI_H
+
+// ============================================================
+// ui.h — Fungsi UI/UX Bersama
+// ============================================================
+
+#include "materi.h"
+#include "linked_list.h"
+
+void prosesPilihMateri(Materi* arr[], int count, PlaylistLinkedList* playlist);
+
+#endif // UI_H
